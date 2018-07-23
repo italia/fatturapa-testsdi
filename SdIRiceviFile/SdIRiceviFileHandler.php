8102b54c6b558115a36618fea3288261d92f3b0d
marcopeca <marco.peca88@gmail.com>
2018-07-23 18:24:13 +0100

    Classe per gestire gli endpoint

---
+++ SdIRiceviFile/SdIRiceviFileHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("autoload.php");
+
+class SdIRiceviFileHandler{
+
+    public function RiceviFile(fileSdIBase_Type $parametersIn)
+    {
+        var_dump($parametersIn);
+        return "ok";
+    }
+
+}
