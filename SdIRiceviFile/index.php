8c599c17324ff0f57d6520851abfe78626026178
marcopeca <marco.peca88@gmail.com>
2018-07-23 18:25:10 +0100

    Server Soap SdIRiceviFile

--- SdIRiceviFile/index.php
+++ SdIRiceviFile/index.php
@@ -1,1 +1,8 @@
 <?php
+
+require_once("../config.php");
+require_once("SdIRiceviFileHandler.php");
+
+$srv = new SoapServer('SdIRiceviFile_v1.0.wsdl');
+$srv->setClass("SdIRiceviFileHandler");
+$srv->handle();
