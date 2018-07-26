007e5c10375b13ac069ef3d33d7171dccb7b28c4
Marco Peca <marco.peca88@gmail.com>
2018-07-26 22:18:33 +0200

    Simple RicezioneFattureHandler

--- RicezioneFatture/RicezioneFattureHandler.php
+++ RicezioneFatture/RicezioneFattureHandler.php
@@ -4,6 +4,13 @@ require_once("autoload.php");
 
 class RicezioneFattureHandler{
 
+    public function RiceviFatture(fileSdIConMetadati_Type $parametersIn){
+        return new \esitoRicezione_Type\ER01;
+    }
+
+    public function NotificaDecorrenzaTermini(fileSdI_Type $parametersIn){
+        
+    }
     
 
 }
