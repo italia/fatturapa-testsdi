90aa7ac1bfc49c8e0dbc261575eacdeee0f63f59
marcopeca <marco.peca88@gmail.com>
2018-07-27 07:37:39 +0100

    TrasmissioneFatture -> function RicevutaConsegna

--- TrasmissioneFatture/TrasmissioneFattureHandler.php
+++ TrasmissioneFatture/TrasmissioneFattureHandler.php
@@ -4,8 +4,12 @@ require_once("autoload.php");
 
 class TrasmissioneFattureHandler{
     
-    public function RicevutaConsegna(){
-
+    public function RicevutaConsegna(fileSdI_Type $parametersIn){
+        $json = json_decode(file_get_contents(ROOT . DB_FILE),TRUE);
+        $IdentificativoSdI = $parametersIn.IdentificativoSdI;
+        $stato = 4;
+        $json[$IdentificativoSdI]["stato"] = $stato;
+        file_put_contents(ROOT . DB_FILE, json_encode($json));
     }
 
     public function NotificaMancataConsegna(){
