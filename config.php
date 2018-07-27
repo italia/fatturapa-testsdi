57be3c1f1f97869fd4164c9ef16df5ef34fd83ba
marcopeca <marco.peca88@gmail.com>
2018-07-27 07:42:43 +0100

    Aggiunte costanti stati fatture

--- config.php
+++ config.php
@@ -1,5 +1,12 @@
 <?php
 
-define("HOSTNAME","http://localhost/sdi/");
+define("HOSTNAME","https://teamdigitale1.simevo.com/sdi/");
 define("ROOT", $_SERVER['DOCUMENT_ROOT'] . "/sdi/");
 define("DB_FILE","test.json");
+
+// STATI FATTURE SDI
+define("FATTURA_RICEVUTA",1);
+define("FATTURA_SCARTATA",2);
+define("NOTIFICA_SCARTATA",3);
+define("FATTURA_ACCETTATA",4);
+define("FATTURA_MANCATA_CONSEGNA",5);
