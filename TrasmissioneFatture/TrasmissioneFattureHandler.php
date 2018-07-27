746dc1225df09baf9c7f485930b6cac4a9439e27
marcopeca <marco.peca88@gmail.com>
2018-07-27 07:06:59 +0100

    generate funzioni TrasmissioneFattureHandler

--- TrasmissioneFatture/TrasmissioneFattureHandler.php
+++ TrasmissioneFatture/TrasmissioneFattureHandler.php
@@ -4,4 +4,28 @@ require_once("autoload.php");
 
 class TrasmissioneFattureHandler{
     
+    public function RicevutaConsegna(){
+
+    }
+
+    public function NotificaMancataConsegna(){
+        
+    }
+
+    public function NotificaScarto(){
+        
+    }
+
+    public function NotificaEsito(){
+        
+    }
+
+    public function NotificaDecorrenzaTermini(){
+        
+    }
+
+    public function AttestazioneTrasmissioneFattura(){
+        
+    }
+
 }
