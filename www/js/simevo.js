217962e51a4d17764acc524d70f8d29de8dd9c59
Riccardo Mariani <riccardo.mariani@gmail.com>
2018-08-15 13:08:06 +0200

    template twig

--- www/js/simevo.js
+++ www/js/simevo.js
@@ -26,19 +26,26 @@ Vue.component('invoice-table', {
     request.send();
   },
   template: '\
-    <div>\
-      <h3>{{ title }}</h3>\
-      <table>\
-        <tr><th>Id</th><th>Da</th><th>A</th></tr>\
-        <tr v-for="i in invoices">\
-            <td>{{ i.id }}</td>\
-            <td>{{ i.source }}</td>\
-            <td>{{ i.destination }}</td>\
-        </tr>\
-      </table>\
-    </div>'
+  <div class="card mb-3">\
+  <div class="card-header">\
+  <i class="fas fa-table"></i> {{ title }}</div>\
+  <div class="card-body">\
+  <div class="table-responsive">\
+  <div>\
+  <table class="table table-bordered" width="100%" cellspacing="0">\
+  <thead><tr><th>Id</th><th>Da</th><th>A</th></tr></thead>\
+  <tfoot><tr v-for="i in invoices">\
+  <td>{{ i.id }}</td>\
+  <td>{{ i.source }}</td>\
+  <td>{{ i.destination }}</td>\
+  </tr></tfoot>\
+  </table>\
+  </div>\
+  </div>\
+  </div>\
+  <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>\
+  </div>'
 });
-
 var app = new Vue({
   el: '#tables'
 });
