f736217e02588680ede71d227ece0cc92457d1bb
Riccardo Mariani <riccardo.mariani@gmail.com>
2018-09-25 11:45:07 +0200

    uplod form

--- www/js/InvoiceTable.js
+++ www/js/InvoiceTable.js
@@ -63,3 +63,23 @@ Vue.component('invoice-table', {
     </div>\
 </div>'
 });
+
+
+// Show filename, show clear button and change browse 
+//button text when a valid extension file is selected
+$(".browse-button input:file").change(function (){
+  $("input[name='attachment']").each(function() {
+    var fileName = $(this).val().split('/').pop().split('\\').pop();
+    $(".filename").val(fileName);
+    $(".browse-button-text").html('<i class="fa fa-refresh"></i> Change');
+    $(".clear-button").show();
+  });
+});
+
+//actions happening when the button is clicked
+$('.clear-button').click(function(){
+    $('.filename').val("");
+    $('.clear-button').hide();
+    $('.browse-button input:file').val("");
+    $(".browse-button-text").html('<i class="fa fa-folder-open"></i> Browse'); 
+}); 
