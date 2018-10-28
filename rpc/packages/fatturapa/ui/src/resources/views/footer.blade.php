0ee50bc631a0c69cddeb1e8e5962d8fd6dacdc55
Paolo Greppi <paolo.greppi@libpf.com>
2018-10-28 17:37:45 +0100

    move footer to footer bland & update link

--- rpc/packages/fatturapa/ui/src/resources/views/footer.blade.php
+++ rpc/packages/fatturapa/ui/src/resources/views/footer.blade.php
@@ -1,2 +1,11 @@
+    <!-- Sticky Footer -->
+    <footer class="sticky-footer">
+        <div class="container my-auto">
+            <div class="text-center my-auto">
+              <span class="copyright">
+                &copy; Copyright 2018 by <a target="_blank" href="https://simevo.com/">simevo.com</a> - <a target="_blank" href="https://github.com/italia/fatturapa-testsdi">source code available here</a></span>
+            </div>
+        </div>
+    </footer>
   </body>
 </html>
--- rpc/packages/fatturapa/ui/src/resources/views/sdi.blade.php
+++ rpc/packages/fatturapa/ui/src/resources/views/sdi.blade.php
@@ -97,15 +97,6 @@
             <script type="text/javascript" src="js/common.js"></script>
     <script type="text/javascript" src="js/InvoiceTable.js"></script>
     <script type="text/javascript" src="js/InvoiceTable2.js"></script>
-    <!-- Sticky Footer -->
-    <footer class="sticky-footer">
-        <div class="container my-auto">
-            <div class="text-center my-auto">
-              <span class="copyright">
-                &copy; Copyright 2018 by <a target="_blank" href="https://simevo.com/">simevo.com</a> - <a target="_blank" href="https://github.com/simevo/fatturapa-testui">source code available here</a></span>
-            </div>
-        </div>
-    </footer>
     
     <script type="text/javascript">
       var app = new Vue({
