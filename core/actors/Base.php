293e500321cf07dbeefc74fcce2cff536c2b5e48
AndreaMauro@Home <bibendus1983@gmail.com>
2018-11-16 11:43:32 +0100

    hotfix for artisan

--- core/actors/Base.php
+++ core/actors/Base.php
@@ -110,8 +110,12 @@ class Base
         if (class_exists('\URL')) {
             // we're inside Laravel: URL is defined in rpc/config/app.php
             $url = \URL::current();
-            $urlData = explode("/", $url);
-            $actor = @$urlData[3];
+            if (PHP_SAPI == 'cli') {
+                $actor = "sdi"; // Quick hotfix, should be configurable
+            } else {
+                $urlData = explode("/", $url);
+                $actor = @$urlData[3];
+            }
         } else {
             $url = $_SERVER['REQUEST_URI'];
             $urlData = explode("/", $url);
